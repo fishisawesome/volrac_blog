d2fe5bf78f075bffc9ca3b58f3152f6c9e810520
fish@fishisawesome.com <fish@fishisawesome.com>
2017-03-21 10:42:46 +0800

    update paths to environment vars

--- settings/production.py
+++ settings/production.py
@@ -124,14 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 ]
 
-STATIC_ROOT = '/home/vblog/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.environ.get('STATIC_ROOT')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT')
 
 # Logging configs
 
