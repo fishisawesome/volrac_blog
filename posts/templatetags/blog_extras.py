7c7b9bc832bc630e59e5f0e7396458c7a1aa1c42
fish@fishisawesome.com <fish@fishisawesome.com>
2017-04-02 08:54:32 +0800

    moved comment templates to their own block include. fixed template tag has_group bug. added social auth profile link. added social auth avatar img

--- posts/templatetags/blog_extras.py
+++ posts/templatetags/blog_extras.py
@@ -4,4 +4,8 @@ register = template.Library()
 
 @register.filter(name='has_group') 
 def has_group(user, group_name):
-    return user.is_superuser or user.groups.objects.filter(name=group_name).exists()
+    return user.is_superuser or user.groups.filter(name=group_name).exists()
+
+@register.filter(name='full_or_username')
+def full_or_username(user):
+    return user.first_name + ' ' + user.last_name if user.first_name != '' and user.last_name != '' else user.username
