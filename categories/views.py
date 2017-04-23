761bd6eabffaa7f8a69f07f056c0d5b5c402044c
fish@fishisawesome.com <fish@fishisawesome.com>
2017-04-23 18:55:56 +0800

    added order_by in category pages

--- categories/views.py
+++ categories/views.py
@@ -6,5 +6,5 @@ def category(request, slug):
     context = {}
     category = get_object_or_404(Category, slug=slug)
     context['category'] = category
-    context['posts'] = category.post_set.all()
+    context['posts'] = category.post_set.order_by('-pub_date').all()
     return render(request, 'categories/detail.html', context)
